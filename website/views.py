e8a0d8b6c4de7eaeb813b3d44abe2d9fb097fa34
denys <denys@hennessey.com>
2021-12-21 23:54:34 +0200

    lab 13

--- website/views.py
+++ website/views.py
@@ -5,7 +5,10 @@ from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 import json
 from website.serializers import *
-
+from rest_framework import generics
+from rest_framework import mixins
+from rest_framework.response import Response
+from rest_framework import status
 from django.http import HttpResponseNotAllowed
 from django.utils.log import log_response
 from functools import wraps
@@ -87,3 +90,45 @@ def put_sto(request):
 
         Sto.objects.create(name=name, user=User.objects.first())
         return JsonResponse({"success": True})
+
+
+class StoList(generics.ListCreateAPIView):
+    queryset = Sto.objects.all()
+    serializer_class = StoSerializer
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset().all()
+        
+        queryset = list(filter(lambda x: x._STATE.search(), queryset))
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+class StoDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Sto.objects.all()
+    serializer_class = StoSerializer
+    def _check(self, request):
+        instance = self.get_object()
+        if ['status'] == list(request.data.keys()):
+            if not instance._STATE.toggle(get_STATE_by_name(request.data['status'])()):
+                raise Exception("Cannot toggle status")
+        else:
+            if 'status' in request.data.keys():
+                raise Exception("Cannot edit status with other data")
+            if not instance._STATE.edit():
+                raise Exception("Sto not editable")
+        
+    def patch(self, request, *args, **kwargs):
+        try:
+            self._check(request)
+        except Exception as e:
+            return Response({"detail": e.__str__()}, status=status.HTTP_403_FORBIDDEN)
+        return super().patch(request, *args, **kwargs)
+
+class ReviewList(generics.ListCreateAPIView):
+    queryset = Review.objects.all()
+    serializer_class = Review
+
+class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Review.objects.all()
+    serializer_class = Review
